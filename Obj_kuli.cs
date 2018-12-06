8def99c24a87ad0062b510b75876c3441bc80151
K2Gendosz <44553325+K2Gendosz@users.noreply.github.com>
2018-12-06 22:24:48 +0100

    Add files via upload
    zad1.4

---
+++ Obj_kuli.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Histogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r;
+            while(true)
+            {
+                Console.Write("Podaj promień kuli : ");
+                if (double.TryParse(Console.ReadLine(), out r))
+                { break; }
+            }
+           
+            double V = (4.0 / 3.0) * Math.PI * r * r * r;
+
+            Console.WriteLine("Obiętość kuli wynoci {0:#.##}",V);
+        }
+
+        
+    }
+}
