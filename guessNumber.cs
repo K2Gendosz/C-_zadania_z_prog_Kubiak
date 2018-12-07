8d8c2b569cd489b967f1aa8f4391b0dac3827f8f
K2Gendosz <44553325+K2Gendosz@users.noreply.github.com>
2018-12-07 23:47:14 +0100

    zadanie 2.5

---
+++ guessNumber.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Komputer wylosował liczbę z przedziału 1 - 9 ");
+            Console.WriteLine("Spróbuj zgadnąć jaka to liczba ");
+            int rand, input;
+            input = wprowadzanie();
+            rand = losowanie();
+            while(!sprawdzanie(input,rand))
+            {
+                Console.Write("Niestety nie zgadłeś sprubój jeszcze raz  ");
+                if (rand > input) { Console.Write("nieco wyżej :D "); }
+                else { Console.Write("nieco niżej :D "); }
+                input = wprowadzanie();
+            }
+            Console.WriteLine("Brawo zgadłeś :D");
+        }
+
+
+        static int wprowadzanie()
+        {
+            int x;
+            while (true)
+            {
+                Console.Write("Wprowadź liczbę z przedziału 1 - 9 : ");
+                if (int.TryParse(Console.ReadLine(), out x)&&x<=9&&x>=1) { return x; }
+                else { Console.WriteLine("Błędna wartość"); }
+            }
+        }
+
+
+        static int losowanie()
+        {
+            Random x = new Random();
+            return x.Next(1,10);
+        }
+
+        static bool sprawdzanie(int input,int rand)
+        {
+            if (input == rand) { return true; }
+            else { return false; }
+
+        }
+
+
+    }
+}
+
+
+
+
+
