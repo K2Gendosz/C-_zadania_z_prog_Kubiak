fd1903b6335da07bf96e5b603826f0ae99ee8d1d
K2Gendosz <44553325+K2Gendosz@users.noreply.github.com>
2018-12-16 23:11:00 +0100

    Add files via upload

--- dzial3_petle_cz_2.cs
+++ dzial3_petle_cz_2.cs
@@ -14,14 +14,22 @@ namespace zadania
 
             // for (){}
             //zadanie3_16();
-            zadanie3_19();
+            //zadanie3_19();
             //zadanie3_22();
+            //zadanie3_25();
+            zadanie3_28();
             //-----------------------------
             // do{}while()
             //zadanie3_17();
+            //zadanie3_20();
+            //zadanie3_23();
+            //zadanie3_26();
             //-----------------------------
             //while(){}
             //zadanie3_18();
+            //zadanie3_21();
+            //zadanie3_24();
+            //zadanie3_27();
             //-----------------------------
 
         }
@@ -35,7 +43,7 @@ namespace zadania
             int[] tab;
             for (int i = 0; i != 2;)
             {
-                if (int.TryParse(Console.ReadLine(), out num)) {  break; }
+                if (int.TryParse(Console.ReadLine(), out num)) { break; }
                 else { Console.Write("Wrong operand.Please try one more time : "); }
             }
             tab = new int[num];
@@ -97,7 +105,7 @@ namespace zadania
         {
             Console.Write("How many number i have to draw : ");
             double avg = 0;
-            int num, min, max,counter=0;
+            int num, min, max, counter = 0;
             int[] tab;
             while (true)
             {
@@ -106,9 +114,9 @@ namespace zadania
             }
             tab = new int[num];
             Random rand = new Random();
-            while (counter<tab.Length)
+            while (counter < tab.Length)
             {
-                tab[counter] = rand.Next(1,100);
+                tab[counter] = rand.Next(1, 100);
                 avg += tab[counter];
                 counter++;
             }
@@ -135,8 +143,51 @@ namespace zadania
         //-----------------------------
         static void zadanie3_19()
         {
-            // do skończenia :)
+            Console.WriteLine("multiplication table 1 - 10 ");
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.Write($"{i * j} \t");
+                }
+                Console.WriteLine();
+            }
+        }
 
+        //-----------------------------
+        static void zadanie3_20()
+        {
+            Console.WriteLine("multiplication table 1 - 10 ");
+            int a = 1, b = 1;
+            do
+            {
+                b = 1;
+                do
+                {
+                    Console.Write($"{a * b} \t");
+                    b++;
+                } while (b <= 10);
+                Console.WriteLine();
+                a++;
+            } while (a <= 10);
+        }
+
+        //-----------------------------
+        static void zadanie3_21()
+        {
+            Console.WriteLine("multiplication table 1 - 10 ");
+            int a = 1, b = 1;
+            while (a <= 10)
+            {
+                b = 1;
+                while (b <= 10)
+                {
+                    Console.Write($"{a * b} \t");
+                    b++;
+                }
+                Console.WriteLine();
+                a++;
+            }
         }
 
         //-----------------------------
@@ -158,9 +209,126 @@ namespace zadania
             Console.WriteLine("");
 
         }
+
+        static void zadanie3_23()
+        {
+            char i = 'A';
+            do
+            {
+                Console.Write($"{i} ");
+                i++;
+            } while (i <='Z');
+            Console.WriteLine();
+
+            do
+            {
+                i--;
+                Console.Write($"{i} ");
+                
+            } while (i>'A');
+
+        }
+
+        static void zadanie3_24()
+        {
+            char i = 'A';
+            bool rev = false;
+            while (true)
+            {
+                Console.Write($"{i} ");
+
+                if (rev == false)
+                { i++; }
+                if (rev == true)
+                { i--; }
+                if (i > 'Z')
+                { rev = true;i--; Console.WriteLine(); }
+                else if (i < 'A'&&rev==true)
+                { break; }
+            }
+            Console.WriteLine();
+        }
+
+        static void zadanie3_25()
+        {
+            Console.WriteLine("We known that 12^2+33^2=1233.");
+            Console.WriteLine("How many similar numbers are in 1000-9999");
+            int numbers = 0;
+            for (int i = 1000; i < 10000; i++)
+            {
+                int a = i / 100;
+                int b = i % 100;
+                if ((a*a)+(b*b)==i)
+                {
+                    numbers++;
+                    Console.WriteLine($"{a}^2 + {b}^2 = {i}");
+                }
+                
+
+            }
+            Console.WriteLine($"Found {numbers} numbers.");
+        }
+        
+        static void zadanie3_26()
+        {
+            int numbs = 0;
+            int number = 1000;
+            Console.WriteLine("We known that 12^2+33^2=1233.");
+            Console.WriteLine("How many similar numbers are in 1000-9999");
+            do
+            {
+                double a = number / 100;
+                double b = number % 100;
+                if (Math.Pow(a, 2) + Math.Pow(b, 2) == number)
+                {
+                    Console.WriteLine($"{a}^2+{b}^2=={number}");
+                    numbs++;
+                }
+
+                    number++;
+            } while (number < 10000);
+            Console.WriteLine($"The program has found {numbs} numbers ");
+        }
+
+        static void zadanie3_27()
+        {
+            int numbs = 0;
+            int number = 1000;
+            Console.WriteLine("We known that 12^2+33^2=1233.");
+            Console.WriteLine("How many similar numbers are in 1000-9999");
+            while(number<10000)
+            {
+                double a = number / 100;
+                double b = number % 100;
+                if (Math.Pow(a,2)+Math.Pow(b,2)==number)
+                {
+                    Console.WriteLine($"{a}^2+{b}^2=={number}");
+                    numbs++;
+                }
+
+                    number++;
+            }
+            Console.WriteLine($"The program has found {numbs} numbers ");
+        }
+
+        static void zadanie3_28()
+        {
+            Console.WriteLine("We known that 990^2+100^2=990100.");
+            Console.WriteLine("How many similar numbers are in 100000-999999");
+            int numbs = 0;
+            for (int i = 100000 ; i < 999999; i++)
+            {
+                int a = i / 1000;
+                int b = i % 1000;
+                if ((a*a)+(b*b)==i)
+                {
+                    Console.WriteLine($"{a}^2+{b}^2=={i}");
+                    numbs++;
+                }
+               
+            }
+            Console.WriteLine($"The program has found {numbs} numbers ");
+        }
     }
 }
-
-
-
 
