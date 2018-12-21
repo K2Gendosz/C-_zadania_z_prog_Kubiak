9bfcba8bfe2f87515c8b9ce4e8c772fe09c4b71d
K2Gendosz <44553325+K2Gendosz@users.noreply.github.com>
2018-12-21 16:27:04 +0100

    Add files via upload

---
+++ MonteCarloPiFinding.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadania
+{
+    class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            int r,points,inCircle=0;
+            Console.Write("Please insert a 'r' value : ");
+
+            Random rand = new Random();
+            
+
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out r))
+                { break; }
+                else
+                {
+                    Console.Write("Wrong operand.Please try one more time : ");
+                }
+                
+            }
+            Console.Write("How many points you want :  ");
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out points))
+                { break; }
+                else
+                {
+                    Console.Write("Wrong operand.Please try one more time : ");
+                }
+
+            }
+            for (int i = 0; i < points; i++)
+            {
+              int XValueOfRand = rand.Next(0,r+1);
+              int YValueOfRand = rand.Next(0, r + 1);
+
+              
+
+                double lenght = (Math.Sqrt( ((XValueOfRand* XValueOfRand) + (YValueOfRand* YValueOfRand)) ) );
+                if (lenght<=(double)r)
+                {
+                    inCircle++;
+                }
+               
+            }
+
+            double pi=4.0*((double)inCircle/(double)points);
+
+            Console.WriteLine(inCircle);
+
+            Console.WriteLine($"Pi = {pi}");
+
+        }
+        
+          
+    }
+
+
+}
+
+
+
+
