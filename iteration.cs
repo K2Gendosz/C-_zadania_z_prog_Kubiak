9c4dfa4b79811b11f435afb1baf1179d75c239bb
K2Gendosz <44553325+K2Gendosz@users.noreply.github.com>
2018-12-22 23:14:05 +0100

    Add files via upload

---
+++ iteration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadania
+{
+    class Program
+    {
+        //00112233445566778899
+        
+        static void Main(string[] args)
+        {
+            int a = 0;
+            for (int i = 1; i < 21; i++)
+            {
+                
+                Console.Write(a);
+                if (i%2==0)
+                    {
+                        a++;
+                    }
+            }
+            Console.WriteLine();
+        }
+        
+          
+    }
+
+
+}
+
+
+
+
